5f00ddc5740d23e322588668363774d0f72ec67e
Stefan Heinzer <stefan@heinzer.io>
2025-08-27 23:23:34 +0200

    Improve event logger

--- src/main/java/com/example/app/_infrastructure/logging/EventLogger.java
+++ src/main/java/com/example/app/_infrastructure/logging/EventLogger.java
@@ -5,37 +5,26 @@ import com.example.app.common.model.Command;
 import lombok.extern.slf4j.Slf4j;
 import org.jmolecules.event.types.DomainEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import static com.example.app.common.logging.LogHelper.getLevel;
+
 @Slf4j
-// @Aspect see comment below
 @Component
 public class EventLogger {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     void on(DomainEvent event) {
-        log.info(LogPrefix.EVENT.withText("" + event));
+        log.atLevel(getLevel(event)).log(LogPrefix.EVENT.withText("" + event));
     }
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     void on(Command command) {
-        log.info(LogPrefix.COMMAND.withText("" + command));
+        log.atLevel(getLevel(command)).log(LogPrefix.COMMAND.withText("" + command));
     }
-
-    /*
-    It would be desirable to log the event _before_ any listener has
-    been invoked, to see the log before other logs induced by event
-    processing. Unfortunately, the join point is not picked up by
-    Spring, possibly because the event multicaster is not being proxied.
-
-    @Before("execution(* multicastEvent(event, ..)) && args(event, ..))")
-    public void log(ApplicationEvent event) {
-        if (event instanceof DomainEvent domainEvent) {
-            log.info(LogPrefix.EVENT.withText("" + domainEvent));
-        } else if (event instanceof Command command) {
-            log.info(LogPrefix.COMMAND.withText("" + command));
-        }
-    }
-     */
 
 }
